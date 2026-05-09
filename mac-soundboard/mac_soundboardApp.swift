4f6268e6ec12d32f04609d14fd730336b674d64a
ByteV0rtex <dorya12@icloud.com>
2026-05-09 14:41:54 +0300

    Initial Commit

---
+++ mac-soundboard/mac_soundboardApp.swift
@@ -0,0 +1,17 @@
+//
+//  mac_soundboardApp.swift
+//  mac-soundboard
+//
+//  Created by Doruk Arpali on 9.05.2026.
+//
+
+import SwiftUI
+
+@main
+struct mac_soundboardApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ mac-soundboardTests/mac_soundboardTests.swift
@@ -0,0 +1,17 @@
+//
+//  mac_soundboardTests.swift
+//  mac-soundboardTests
+//
+//  Created by Doruk Arpali on 9.05.2026.
+//
+
+import Testing
+@testable import mac_soundboard
+
+struct mac_soundboardTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
